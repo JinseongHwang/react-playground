da301ecdb8691e249c0cacabf2c0ba6219ebf4b2
JinseongHwang <jinseong.dev@gmail.com>
2023-07-18 02:05:50 +0900

    feat: save diary with props

--- diary-app/src/App.js
+++ diary-app/src/App.js
@@ -1,36 +1,30 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList"
+import {useRef, useState} from "react";
 
-const dummyList = [
-    {
-        id: 1,
-        author: "jinseong1",
-        content: "hello world1",
-        emotion: 1,
-        created_date: new Date().getTime()
-    },
-    {
-        id: 2,
-        author: "jinseong2",
-        content: "hello world2",
-        emotion: 1,
-        created_date: new Date().getTime()
-    },
-    {
-        id: 3,
-        author: "jinseong3",
-        content: "hello world3",
-        emotion: 1,
-        created_date: new Date().getTime()
-    },
-]
+const App = () => {
+    const [data, setData] = useState([])
 
-function App() {
+    const dataId = useRef(0)
+
+    const onCreate = (author, content, emotion) => {
+        const created_date = new Date().getTime()
+        const newItem = {
+            author,
+            content,
+            emotion,
+            created_date,
+            id: dataId.current,
+        };
+        dataId.current += 1;
+        setData([newItem, ...data]);
+    };
+
     return (
         <div className="App">
-            <DiaryEditor/>
-            <DiaryList diaryList={dummyList}/>
+            <DiaryEditor onCreate={onCreate}/>
+            <DiaryList diaryList={data}/>
         </div>
     );
 }
--- diary-app/src/DiaryEditor.js
+++ diary-app/src/DiaryEditor.js
@@ -1,6 +1,6 @@
 import {useRef, useState} from "react";
 
-const DiaryEditor = () => {
+const DiaryEditor = ({onCreate}) => {
     const authorInput = useRef()
     const contentInput = useRef()
 
@@ -27,7 +27,16 @@ const DiaryEditor = () => {
             return;
         }
 
+        // 저장 event
+        onCreate(state.author, state.content, state.emotion)
         alert("저장 성공!");
+
+        // 입력칸 초기화
+        setState({
+            author: "",
+            content: "",
+            emotion: 1,
+        });
     }
 
     return (
