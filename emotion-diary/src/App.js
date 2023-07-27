5865e964beef3433a53428d98255b10f87bb6705
JinseongHwang-Kurly <jinseong.hwang@kurlycorp.com>
2023-07-28 01:58:18 +0900

    feat: home page

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -31,8 +31,41 @@ const reducer = (state, action) => {
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
 
+const dummyData = [
+    {
+        id: 1,
+        emotion: 1,
+        content: "오늘의 일기 1번",
+        date: 1690131852000
+    },
+    {
+        id: 2,
+        emotion: 5,
+        content: "오늘의 일기 2번",
+        date: 1690131852012
+    },
+    {
+        id: 3,
+        emotion: 2,
+        content: "오늘의 일기 3번",
+        date: 1690131851012
+    },
+    {
+        id: 4,
+        emotion: 3,
+        content: "오늘의 일기 4번",
+        date: 1690131952012
+    },
+    {
+        id: 5,
+        emotion: 4,
+        content: "오늘의 일기 5번",
+        date: 1690131951912
+    },
+]
+
 function App() {
-    const [data, dispatch] = useReducer(reducer, []);
+    const [data, dispatch] = useReducer(reducer, dummyData);
 
     const dataId = useRef(0);
 
--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import {DiaryStateContext} from "../App";
+import DiaryList from "../components/DiaryList";
 
 const Home = () => {
+    const diaryList = useContext(DiaryStateContext);
+
+    const [data, setData] = useState([]);
+    const [currDate, setCurrDate] = useState(new Date());
+
+    useEffect(() => {
+        if (diaryList.length >= 1) {
+            const firstDay = new Date(currDate.getFullYear(), currDate.getMonth(), 1).getTime();
+            const lastDay = new Date(currDate.getFullYear(), currDate.getMonth() + 1, 0).getTime();
+            setData(diaryList.filter(it => firstDay <= it.date && it.date <= lastDay));
+        }
+    }, [diaryList, currDate]);
+
+    const headDate = `${currDate.getFullYear()}년 ${currDate.getMonth() + 1}월`;
+
+    const increaseMonth = () => {
+        setCurrDate(new Date(currDate.getFullYear(), currDate.getMonth() + 1, currDate.getDate()));
+    }
+    const decreaseMonth = () => {
+        setCurrDate(new Date(currDate.getFullYear(), currDate.getMonth() - 1, currDate.getDate()));
+    }
+
     return (
         <div>
-            <h1>Home</h1>
-            <p>이곳은 홈 입니다.</p>
+            <MyHeader
+                headText={headDate}
+                leftChild={<MyButton text={"<"} onClick={decreaseMonth}/>}
+                rightChild={<MyButton text={">"} onClick={increaseMonth}/>}
+            />
+            <DiaryList diaryList={data}/>
         </div>
     );
 };
