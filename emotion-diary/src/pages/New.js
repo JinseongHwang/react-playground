8dacb230a5194def071319fb3fbf415716bff395
JinseongHwang <jinseong.dev@gmail.com>
2023-07-28 10:01:02 +0900

    feat: new page

--- emotion-diary/src/pages/New.js
+++ emotion-diary/src/pages/New.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import MyHeader from "../components/MyHeader";
+import MyButton from "../components/MyButton";
+import {useNavigate} from "react-router-dom";
+import DiaryEditor from "../components/DiaryEditor";
+
+const getStringDate = (date) => {
+    return date.toISOString().slice(0, 10);
+}
 
 const New = () => {
     return (
         <div>
-            <h1>New</h1>
-            <p>이곳은 일기 작성 페이지 입니다.</p>
+            <DiaryEditor/>
         </div>
-    );
+    )
 };
 
 export default New;
