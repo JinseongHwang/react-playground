e7809282335f9c749a37a5bcda6d28abd4d06b43
JinseongHwang <jinseong.dev@gmail.com>
2023-11-17 04:03:17 +0900

    feat: edit cell

--- mui-datagrid/src/App.tsx
+++ mui-datagrid/src/App.tsx
@@ -7,6 +7,12 @@ type TableRow = {
   "age": number,
 }
 
+type CellEditProps = {
+  "rowId": string,
+  "colName": string,
+  "value": string,
+}
+
 const initialData: TableRow[] = [
   {"name": "jinseong", "age": 20},
   {"name": "gildong", "age": 30},
@@ -25,12 +31,39 @@ const App = () => {
     id: idx, ...row
   }));
 
+  const parseCellEditProps = (e: any): CellEditProps | null => {
+    const editingRowId = Object.keys(e.editRows)[0]
+    if (editingRowId === undefined) return null;
+    const editingColName = Object.keys(e.editRows[editingRowId])[0]
+    const editingValue = e.editRows[editingRowId][editingColName].value
+    return {
+      "rowId": editingRowId,
+      "colName": editingColName,
+      "value": editingValue,
+    }
+  }
+
+  const handleEditEvent = (e: any) => {
+    const editProps = parseCellEditProps(e);
+    if (editProps === null) return;
+
+    setData(prevState => {
+      return prevState.map((row, idx) => {
+        if (idx.toString() === editProps.rowId) {
+          return {...row, [editProps.colName]: editProps.value};
+        }
+        return row;
+      });
+    });
+  };
+
   return (
     <div className="App">
       <DataGrid
         sx={{"width": "500px", "margin": "auto"}}
         columns={columns}
         rows={rows}
+        onStateChange={handleEditEvent}
       />
     </div>
   );
