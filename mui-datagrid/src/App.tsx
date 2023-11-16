ab857829768d4dbe7bb2a3d281e227eb7f03f082
JinseongHwang <jinseong.dev@gmail.com>
2023-11-17 02:53:47 +0900

    feat: add data state

--- mui-datagrid/src/App.tsx
+++ mui-datagrid/src/App.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {DataGrid, GridColDef, GridRowsProp} from "@mui/x-data-grid";
 
-const initialData = [
+type TableRow = {
+  "name": string,
+  "age": number,
+}
+
+const initialData: TableRow[] = [
   {"name": "jinseong", "age": 20},
   {"name": "gildong", "age": 30},
   {"name": "cheolsu", "age": 40},
 ]
 
 const App = () => {
+  const [data, setData] = useState<TableRow[]>(initialData)
+
   const columns: GridColDef[] = [
     {field: 'name', headerName: 'Name', width: 150},
     {field: 'age', headerName: 'Age', editable: true},
   ];
 
-  const rows: GridRowsProp = initialData.map((data, idx) => ({
-    id: idx, ...data
+  const rows: GridRowsProp = data.map((row, idx) => ({
+    id: idx, ...row
   }));
 
   return (
