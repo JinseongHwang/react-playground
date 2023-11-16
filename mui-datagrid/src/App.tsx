b0d10f2d90140846df71d37148a6e6298c49e1cc
JinseongHwang <jinseong.dev@gmail.com>
2023-11-16 18:57:58 +0900

    feat: set initial data

--- mui-datagrid/src/App.tsx
+++ mui-datagrid/src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.css';
+import {DataGrid, GridColDef, GridRowsProp} from "@mui/x-data-grid";
 
-function App() {
+const initialData = [
+  {"name": "jinseong", "age": 20},
+  {"name": "gildong", "age": 30},
+  {"name": "cheolsu", "age": 40},
+]
+
+const App = () => {
+  const columns: GridColDef[] = [
+    {field: 'name', headerName: 'Name', width: 150},
+    {field: 'age', headerName: 'Age', editable: true},
+  ];
+
+  const rows: GridRowsProp = initialData.map((data, idx) => ({
+    id: idx, ...data
+  }));
+
   return (
     <div className="App">
+      <DataGrid
+        sx={{"width": "500px", "margin": "auto"}}
+        columns={columns}
+        rows={rows}
+      />
     </div>
   );
 }
